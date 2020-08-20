5895b95d0da557df3a869ff7860b8edd43c79011
shivamlatiyan98 <coolshivam686@gmail.com>
2020-08-20 18:31:44 +0530

    Add files via upload

---
+++ assignments/week1/git commands.sh
@@ -0,0 +1,11 @@
+git fetch
+git log
+git reflog
+git status
+git stash
+git push -u origin <branch>
+git pull
+git commit -m"commit message"
+git merge <branch>
+git rebase -i
+git cherry-pick branch
